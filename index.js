5c6a32191a84482b48a651f382925e4923de4366
Hsiaok <kevin521998@gmail.com>
2022-11-04 20:37:51 +0800

    add breed api

--- index.js
+++ index.js
@@ -23,12 +23,25 @@ app.get("/api/cats", (req, res) => {
     .catch((err) => console.log(err));
 });
 
+// set page and order
 app.get("/api/moreCats", (req, res) => {
-  // set page and oreder
   const pageNumber = req.query.pageNumber;
   axios
     .get(
       `https://api.thecatapi.com/v1/images/search?limit=12&page=${pageNumber}&has_breeds=1&order=RAND&api_key=${process.env.THE_CAT_API}`
+    )
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((err) => console.log(err));
+});
+
+// breeds_ids
+app.get("/api/breeds/ids", (req, res) => {
+  const breedsID = req.query.breeds_ids;
+  axios
+    .get(
+      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedsID}&limit=12&order=RAND&api_key=${process.env.THE_CAT_API}`
     )
     .then((response) => {
       res.json(response.data);
